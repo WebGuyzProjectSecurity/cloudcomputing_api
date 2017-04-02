de31f9e69defd0e2a17f3a948f26858b3fa9242e
mayatp <mayapetranova@gmail.com>
2017-04-02 20:20:48 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(name='mtp30apitest1',
       description='OpenShift App',
       author='Maya Petranova',
       author_email='mayapetranova@gmail.com',
-      url='https://www.python.org/community/sigs/current/distutils-sig',
+      url='http://mtp30apitest1-mtp30apitest1.apps.devcloud.eecs.qmul.ac.uk/',
       ##install_requires=['Flask>=0.7.2', 'MarkupSafe'],
       install_requires=['Flask','flask-wtf', 'unirest', 'wtforms','NumPy', 'Six', 'Jinja2', 'Requests', 'Tornado >= 4.0', 'PyYaml', 'DateUtil', 'bokeh==0.12.4', 'Futures', 'Flask-PyMongo', 'Navigation'],
       )
